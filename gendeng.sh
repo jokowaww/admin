679fd38f8a90b3a76af4cd73b4c9453916c3e545
jokowaw <150768441+jokowaww@users.noreply.github.com>
2024-02-08 14:09:14 +0700

    Update gendeng.sh

--- gendeng.sh
+++ gendeng.sh
@@ -14,3 +14,30 @@ cd wongedan
 rm var.txt
 rm database.db
 pip3 install -r requirements.txt
+
+echo -e '#!/bin/bash\ncd /root/\npython3 -m wongedan' > /usr/bin/nenen
+
+
+chmod 777 /usr/bin/nenen
+
+cat > /etc/systemd/system/wongedan.service << END
+[Unit]
+Description=Simple wongedan - @wongedan
+After=network.target
+
+[Service]
+WorkingDirectory=/root
+ExecStart=/usr/bin/nenen
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+
+END
+systemctl daemon-reload
+systemctl start wongedan
+systemctl enable wongedan
+
+cp /tmp/var.txt /root/wongedan
+
+echo " Installations complete, type /menu on your bot "
