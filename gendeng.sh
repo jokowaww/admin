2f93cf59901e4c4559d16cbf9f4b8a696d24943e
jokowaw <150768441+jokowaww@users.noreply.github.com>
2024-02-08 13:53:34 +0700

    Add files via upload

---
+++ gendeng.sh
@@ -0,0 +1,15 @@
+#install
+cp /media/wongedan/var.txt /tmp
+cp /root/wongedan/var.txt /tmp
+rm -rf wongedan
+apt update && apt upgrade -y
+apt install python3 python3-pip -y
+apt install sqlite3 -y
+cd /root/
+rm -rf wongedan
+wget https://raw.githubusercontent.com/jokowaww/admin/main/gendeng.zip
+unzip gendeng.zip
+cd wongedan
+rm var.txt
+rm database.db
+pip3 install -r requirements.txt
