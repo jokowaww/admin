f78759c5ee063b25491cfb891ff3a9265b230a66
jokowaw <150768441+jokowaww@users.noreply.github.com>
2024-02-08 14:00:27 +0700

    Update gendeng.sh

--- gendeng.sh
+++ gendeng.sh
@@ -1,10 +1,11 @@
 #install
-cp /media/wongedan/var.txt /tmp
+cp /root/wongedan/var.txt /tmp
 cp /root/wongedan/var.txt /tmp
 rm -rf wongedan
 apt update && apt upgrade -y
 apt install python3 python3-pip -y
 apt install sqlite3 -y
+pip install python-telegram-bot==13.7
 cd /root/
 rm -rf wongedan
 wget https://raw.githubusercontent.com/jokowaww/admin/main/gendeng.zip
